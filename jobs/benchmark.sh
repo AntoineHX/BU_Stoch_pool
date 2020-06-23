115a8d80b57e17fc7fbdf3966bebde69103662bd
Antoine Harlé <antoh@cedar5.cedar.computecanada.ca>
2020-06-23 08:13:40 -0700

    Cleaner setup

--- jobs/benchmark.sh
+++ jobs/benchmark.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1 #gpu:v100l:1    # https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
+#SBATCH --gres=gpu:1 #v100l:1    # https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
 #SBATCH --cpus-per-task=6 #6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M  #32000M       # Memory proportional to CPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --account=def-mpederso
-#SBATCH --time=1:00:00
-#SBATCH --job-name=MyLeNetMatStochBUNoceil
+#SBATCH --time=10:00:00
+#SBATCH --job-name=Benchmark-MyLeNetMatNormalNoceil
 #SBATCH --output=log/%x-%j.out
 #SBATCH --mail-user=harle.collette.antoine@gmail.com
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
+#SBATCH --array=1-3
+
 
 
 # Setup
 source ~/virtual_env/stoch_pool/bin/activate
 
 #Execute
-# echo $(pwd) = /home/antoh/projects/def-mpederso/antoh/stoch/jobs
 cd ../
 
 time python main.py \
-    -n MyLeNetMatStochBUNoceil \
-    -ep 10 \
+    -n MyLeNetMatNormalNoceil \
+    -ep 100 \
     -sc cosine \
     -lr 5e-2 \
-    -pf _noCrop_k3
+    -rf '../res/benchmark_NoCeil/' \
+    -k 1 \
+    -pf __k1_$SLURM_ARRAY_TASK_ID
--- jobs/setup_env.sh
+++ jobs/setup_env.sh
@@ -9,9 +9,11 @@
 
 # Setup
 #sudo rm -rf ~/dataug/
+cd ~
+mkdir virtual_env
 module load python/3.7.4
-virtualenv --no-download ~/stoch_pool
-source ~/stoch_pool/bin/activate
+virtualenv --no-download ~/virtual_env/stoch_pool
+source ~/virtual_env/stoch_pool/bin/activate
 pip install --no-index --upgrade pip
 
 pip install --no-index torch torchvision #torchviz
